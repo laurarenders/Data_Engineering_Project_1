a46f406747b08714435ab36201186515094c5833
laurarenders <77204794+laurarenders@users.noreply.github.com>
2022-03-16 15:27:16 +0000

    Create mysql.connector.py

---
+++ mysql.connector.py
@@ -0,0 +1,62 @@
+import pandas as pd
+import pymysql
+import logging
+import sshtunnel
+from sshtunnel import SSHTunnelForwarder
+
+ssh_host = "vichogent.be"
+ssh_username = "root"
+ssh_password = "DEPGroep1"
+database_username = "depuser"
+database_password = "DepUser123"
+database_name = "DEP1Database" 
+localhost = '127.0.0.1'
+
+def open_ssh_tunnel(verbose=False):
+    """Open an SSH tunnel and connect using a username and password.
+    
+    :param verbose: Set to True to show logging
+    :return tunnel: Global SSH tunnel connection
+    """
+    
+    if verbose:
+        sshtunnel.DEFAULT_LOGLEVEL = logging.DEBUG
+    
+    global tunnel
+    tunnel = SSHTunnelForwarder(
+        (ssh_host, 40010),
+        ssh_username = ssh_username,
+        ssh_password = ssh_password,
+        remote_bind_address = ('127.0.0.1', 3306)
+    )
+    
+    tunnel.start()
+
+def mysql_connect():
+    """Connect to a MySQL server using the SSH tunnel connection
+    
+    :return connection: Global MySQL database connection
+    """
+    
+    global connection
+    
+    connection = pymysql.connect(
+        host='127.0.0.1',
+        user=database_username,
+        passwd=database_password,
+        db=database_name,
+        port=tunnel.local_bind_port
+    )
+
+def run_query(sql):
+    """Runs a given SQL query via the global database connection.
+    
+    :param sql: MySQL query
+    :return: Pandas dataframe containing results
+    """
+    
+    return pd.read_sql_query(sql, connection)
+
+open_ssh_tunnel()
+mysql_connect()
+run_query("SELECT * FROM Bedrijf")
