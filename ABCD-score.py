455bb72c25218d727ae015857c0acd96512e6db4
laurarenders <77204794+laurarenders@users.noreply.github.com>
2022-05-01 13:56:27 +0100

    Update ABCD-score.py

--- ABCD-score.py
+++ ABCD-score.py
@@ -7,26 +7,23 @@ df2 = pd.read_csv("scores/voorkomensPDF.csv", sep=';')
 
 cols = ['gendergelijkheid', 'implementatie_werknemersrechten', 'sociale_relaties', 'werkgelegenheid', 'organisatie_op_het_werk', 'gezondheid_en_veiligheid', 'opleidingsbeleid', 'gebruik_van_energiebronnen', 'gebruik_van_waterbronnen', 'emissies_van_broeikasgassen', 'vervuilende_uitstoot', 'milieu_impact', 'impact_op_gezondheid_en_veiligheid', 'verdere_eisen_over_bepaalde_onderwerpen', 'milieu_beleid', 'SDGs']
 
-df["tot"] = df[cols].sum(axis=1) 
-df2["tot"] = df2[cols].sum(axis=1) 
+df["totWEB"] = df[cols].sum(axis=1) 
+df["totPDF"] = df2[cols].sum(axis=1) 
 
-df["echttotaal"] = df['tot'] + df2['tot']
+df["totAlles"] = df['totWEB'] + df['totPDF']
 
-# df["perc"] = df["tot"]/110*100
+df["perc"] = df["totAlles"]/110*100
 
-# conditions = [
-#     (df['perc'] < 25),
-#     (df['perc'] >= 25) & (df['perc'] < 50),
-#     (df['perc'] >= 50) & (df['perc'] < 75),
-#     (df['perc'] >= 75)
-#     ]
+conditions = [
+    (df['perc'] < 25),
+    (df['perc'] >= 25) & (df['perc'] < 50),
+    (df['perc'] >= 50) & (df['perc'] < 75),
+    (df['perc'] >= 75)
+    ]
 
-# values = ['D', 'C', 'B', 'A']
+values = ['D', 'C', 'B', 'A']
 
-# df['score'] = np.select(conditions, values)
-
-print(df)
-
-# print(df[['ondnr', 'tot', 'perc', 'score']])
+df['score'] = np.select(conditions, values)
 
 
+print(df[['ondnr', 'perc', 'score']])
