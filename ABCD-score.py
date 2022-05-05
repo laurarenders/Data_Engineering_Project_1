401da1b1109606178b542fab3d7d496235c5cd56
laurarenders <77204794+laurarenders@users.noreply.github.com>
2022-05-05 13:49:13 +0100

    Update ABCD-score.py

--- ABCD-score.py
+++ ABCD-score.py
@@ -2,7 +2,7 @@ import csv
 import  pandas as pd
 import numpy as np
 
-df = pd.read_csv("scores/voorkomensWEB.csv", sep=';')
+df = pd.read_csv("scores/voorkomensWEBUniek.csv", sep=';')
 df2 = pd.read_csv("scores/voorkomensPDF.csv", sep=';')
 
 cols = ['gendergelijkheid', 'implementatie_werknemersrechten', 'sociale_relaties', 'werkgelegenheid', 'organisatie_op_het_werk', 'gezondheid_en_veiligheid', 'opleidingsbeleid', 'gebruik_van_energiebronnen', 'gebruik_van_waterbronnen', 'emissies_van_broeikasgassen', 'vervuilende_uitstoot', 'milieu_impact', 'impact_op_gezondheid_en_veiligheid', 'verdere_eisen_over_bepaalde_onderwerpen', 'milieu_beleid', 'SDGs']
@@ -12,7 +12,8 @@ df["totPDF"] = df2[cols].sum(axis=1)
 
 df["totAlles"] = df['totWEB'] + df['totPDF']
 
-df["perc"] = df["totAlles"]/110*100
+df["perc"] = round(df["totAlles"]/110*100)
+df[" "] = '    '
 
 conditions = [
     (df['perc'] < 25),
@@ -26,4 +27,6 @@ values = ['D', 'C', 'B', 'A']
 df['score'] = np.select(conditions, values)
 
 
-print(df[['ondnr', 'perc', 'score']])
+print(df[['ondnr', ' ', 'perc', ' ', 'score']])
+
+df[['ondnr', ' ', 'perc', ' ', 'score']].to_csv (r'/Users/laurarenders/DEPGroep1-2/ABCDscore.csv', index = False, header=True)
